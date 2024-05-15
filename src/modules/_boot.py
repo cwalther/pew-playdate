a4b4b814feab7fddc11977f74e3c1c117b2c7d4e
Christian Walther <cwalther@gmx.ch>
2024-05-15 18:30:48 +0200

    Create and mount Data/ch.kolleegium.pewpew/Files/ as the default root filesystem.

--- src/modules/_boot.py
+++ src/modules/_boot.py
@@ -1,1 +1,12 @@
-# nothing here yet (will eventually mount the filesystem)
+from _pew import VfsPD
+import os, vfs
+
+vfs.mount(VfsPD(), '/root')
+try:
+	os.stat('/root/Files')
+except OSError:
+	os.mkdir('/root/Files')
+vfs.mount(VfsPD('Files'), '/')
+
+os.umount('/root')
+del VfsPD, os, vfs
